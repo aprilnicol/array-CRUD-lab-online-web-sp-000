3065bac85254d6b4259ec0abc710c9c7ccc2afe9
April Adams <illusionlace@gmail.com>
2020-08-18 09:00:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+array = ["apples", "bananas", "pie", "blueberries"]
+array << "Strawberries"
 end
 
 def add_element_to_start_of_array(array, element)
